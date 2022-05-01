b23c2c1b85f1225f17c4878c8a5434a5803ec911
Ing. Geury Brito <ing.gbrito@gmail.com>
2022-04-30 22:15:19 -0400

    Trabjando ChangePassword

---
+++ BeEntity/BeChangePassword.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeEntity
+{
+
+    /// <summary>
+    /// Classe Change Password
+    /// </summary>
+   public class BeChangePassword
+    {
+
+        [Required(ErrorMessage = "Usuario requerido.")]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "Clave actual(número carnet)")]
+        [StringLength(10, ErrorMessage = "Debe tener mínimio 6 caracteres", MinimumLength = 6)]
+        public string Password  { get; set; }
+        [Required(ErrorMessage = " Nueva Contraseña")]
+        [DataType(DataType.Password)]
+        [StringLength(10, ErrorMessage = "Debe tener mínimio 6 caracteres", MinimumLength = 6)]
+
+        public string NewPassword { get; set; }
+        [Required(ErrorMessage = "Confirmar Contraseña")]     
+        [DataType(DataType.Password)]
+        //[Compare(NewPassword)]
+        [StringLength(10, ErrorMessage = "The password must be atleast 6 characters long", MinimumLength = 6)]
+        [Compare("Password", ErrorMessage = "Password and Confirmation Password must match.")]
+        public string ConfirmPassword { get; set; }
+
+
+    }
+}
---
+++ SistRE/Comun/EnumComun.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistRE.Comun
+{
+
+    /// <summary>
+    /// Enum Comun
+    /// </summary>
+    public class EnumComun
+    {
+
+       public  enum   ChangePassword
+        {
+          
+       Change = 1,
+       NotChange = 0
+
+        }
+    }
+}
